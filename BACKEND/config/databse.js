bb56bc18662feadeedabfdaf0e34457cc66702af
Manikarnikayadav <perlzaid057@gmail.com>
2024-08-23 08:48:09 +0530

    connection done

---
+++ BACKEND/config/databse.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const connectDatabase = ()=>{
+
+    mongoose.connect(process.env.DB_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useCreateIndex:true,// this is no longer supported
+        useFindAndModify: false,
+    }).then((data)=>{
+     console.log(`MongoDB connected with server:${data.Connection.host}`);
+     
+    }).catch((err)=>{
+        console.log(err);
+        
+    })
+}
+
+module.exports = connectDatabase
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,4 +1,16 @@
 const app = require("./app");
+const dotenv = require("dotenv")
+const connectDatabase = require("./config/databse.js")
+
+// config
+
+dotenv.config({path:"BACKEND/config/config.env"})
 
 
-app.listen()
+// Connecting to databse it should be called after config setup
+connectDatabase()
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+    
+})
