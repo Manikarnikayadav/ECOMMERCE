0a60092ca843decd319c572fff4f1b749e162264
Manikarnikayadav <perlzaid057@gmail.com>
2024-08-23 10:07:33 +0530

    Product Controllers, Models, Routing and testing done

--- BACKEND/config/databse.js
+++ BACKEND/config/databse.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose")
 const connectDatabase = ()=>{
 
     mongoose.connect(process.env.DB_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-        useCreateIndex:true,// this is no longer supported
-        useFindAndModify: false,
+        // useNewUrlParser:true,
+        // useUnifiedTopology:true,
+        // useCreateIndex:true,// this is no longer supported
+        // useFindAndModify: false,
     }).then((data)=>{
      console.log(`MongoDB connected with server:${data.Connection.host}`);
      
